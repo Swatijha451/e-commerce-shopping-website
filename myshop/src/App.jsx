6fb88bebf1986c77d60ac9f3727970acd71f3825
Swatijha451 <swatijha451@gmail.com>
2024-05-05 12:18:06 +0530

    set up project

--- myshop/src/App.jsx
+++ myshop/src/App.jsx
@@ -1,9 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeItem from './components/HomeItem';
+
 const App=()=>{
+  const item ={
+    id: '008',
+    image: 'images/8.jpg',
+    company: 'Nivea',
+    item_name: 'Men Fresh Deodrant 150ml',
+    original_price: 285,
+    current_price: 142,
+    discount_percentage: 50,
+    return_period: 14,
+    delivery_date: '10 Oct 2023',
+    rating: {
+        stars: 4.2,
+        count: 5200,
+    },
+  }
   return (
     <>
-    myShop
+    <Header></Header>
+    <main>
+        <div className="items-container">
+        <HomeItem item={item}></HomeItem>
+        </div>
+    </main>
+    <Footer></Footer>
     </>
   )
 };
